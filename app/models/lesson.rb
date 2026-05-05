25f1e5f2fc2d4778f74dfd9c23947ed94741b84c
Josh Obaro <joshuaaob@gmail.com>
2026-05-05 01:05:53 +0100

    add topic to lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,6 +1,7 @@
 # app/models/lesson.rb
 class Lesson < ApplicationRecord
   has_one :audio_file, dependent: :destroy
+  belongs_to :topic, optional: true # remove `optional: true` if you enforced null: false
   validates :title, presence: true
   validates :content, presence: true
 
