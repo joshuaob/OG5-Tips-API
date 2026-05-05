64329628486258ab93064ff5888e6ce888ff8215
Josh Obaro <joshuaaob@gmail.com>
2026-05-05 01:05:20 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_05_04_090159) do
+ActiveRecord::Schema[8.0].define(version: 2026_05_04_221732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,7 +46,17 @@ ActiveRecord::Schema[8.0].define(version: 2026_05_04_090159) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "topic_id", null: false
+    t.index ["topic_id"], name: "index_lessons_on_topic_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "audio_files", "lessons"
+  add_foreign_key "lessons", "topics"
 end
